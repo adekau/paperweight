ce23a8758537d2e576b927f8d8550e380785a51e
Alexander J Dekau <alexander.j.dekau@wmich.edu>
2020-02-19 21:32:22 -0500

    save/get/delete tests

--- projects/forms/src/test/indexed-db.service.spec.ts
+++ projects/forms/src/test/indexed-db.service.spec.ts
@@ -1,6 +1,7 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { IPaperweightOptions } from 'projects/contracts/src/public-api';
+import { switchMap, tap } from 'rxjs/operators';
 
 import { IndexedDBService } from '../lib/indexed-db.service';
 import { PAPERWEIGHT_OPTIONS } from '../lib/paperweight-options';
@@ -25,23 +26,56 @@ describe('Forms: IndexedDBService', () => {
         }).compileComponents();
 
         opts = {
-            storeName: 'testStore',
             debounceInterval: 1
         };
     });
 
     describe('Options', () => {
         it('should use storeName if set', () => {
+            opts.storeName = 'testStore';
             const idbs = TestBed.inject(IndexedDBService);
 
             expect(idbs.storeName).toEqual('testStore');
         });
 
         it('should default to drafts if storeName is not set', () => {
-            opts.storeName = undefined;
             const idbs = TestBed.inject(IndexedDBService);
 
             expect(idbs.storeName).toEqual('drafts');
         });
+    });
+
+    it('should save and retrieve an object', (done) => {
+        const idbs = TestBed.inject(IndexedDBService);
+
+        idbs.put({ test: 'value' })
+            .pipe(
+                switchMap(key => idbs.get(key))
+            )
+            .subscribe({
+                next: draft => {
+                    expect(draft).toEqual({ test: 'value', id: jasmine.anything() });
+                    done();
+                }
+            });
+    });
+
+    it('should delete an object', (done) => {
+        const idbs = TestBed.inject(IndexedDBService);
+        let key: IDBValidKey;
+
+        idbs.put({ test: 'value' })
+            .pipe(
+                tap(k => key = k),
+                switchMap(k => idbs.delete(k)),
+                switchMap(() => idbs.get(key))
+            )
+            .subscribe({
+                next: d => {
+                    expect(key).toBeDefined();
+                    expect(d).toBeUndefined();
+                    done();
+                }
+            });
     });
 });
